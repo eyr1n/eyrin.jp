c21f5192d1cf611ff02834cb0eae2a374e78d3a4
Rin Iwai <rin@eyrin.jp>
2024-07-26 02:25:41 +0900

    katex導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import behead from 'remark-behead';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eyrin.jp',
   integrations: [mdx(), sitemap(), tailwind(), react()],
   markdown: {
-    remarkPlugins: [[behead, { minDepth: 2 }]],
+    remarkPlugins: [[behead, { minDepth: 2 }], remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
